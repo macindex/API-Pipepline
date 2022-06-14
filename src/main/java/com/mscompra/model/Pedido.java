70f9ee42e81ff308a3866381966aa50f8179e339
macindex <machollanda@gmail.com>
2022-06-14 10:07:32 -0300

    validation

--- src/main/java/com/mscompra/model/Pedido.java
+++ src/main/java/com/mscompra/model/Pedido.java
@@ -1,11 +1,12 @@
 package com.mscompra.model;
 
-import java.math.BigDecimal;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "tb_pedido")
@@ -15,61 +16,77 @@ public class Pedido {
 	// @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
 	private Integer Id;
+	@NotBlank
 	@Column(nullable = false, length = 50)
 	private String nome;
+	@NotNull
 	@Column(nullable = false, length = 50)
 	private String produto;
+	@NotNull
+	@Min(1)
 	@Column(nullable = false, length = 50)
 	private Integer valor;
+	@NotNull
 	private String dataCompra;
+	@NotNull
 	private String cpfCliente;
 	private String cep;
-	
-	
+
 	public Integer getId() {
 		return Id;
 	}
+
 	public void setId(Integer id) {
 		Id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getProduto() {
 		return produto;
 	}
+
 	public void setProduto(String produto) {
 		this.produto = produto;
 	}
+
 	public Integer getValor() {
 		return valor;
 	}
+
 	public void setValor(Integer valor) {
 		this.valor = valor;
 	}
+
 	public String getDataCompra() {
 		return dataCompra;
 	}
+
 	public void setDataCompra(String dataCompra) {
 		this.dataCompra = dataCompra;
 	}
+
 	public String getCpfCliente() {
 		return cpfCliente;
 	}
+
 	public void setCpfCliente(String cpfCliente) {
 		this.cpfCliente = cpfCliente;
 	}
+
 	public String getCep() {
 		return cep;
 	}
+
 	public void setCep(String cep) {
 		this.cep = cep;
 	}
-	
-	
 
 	// @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
 //	private String dataCompra;
